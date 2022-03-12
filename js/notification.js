a8e2e413a0e08d24072e2f84b7b8805b723f0393
Vedant Nandwana <vedantnn.confidential@gmail.com>
2022-03-12 21:45:29 +0530

    minor bug fixes.

--- js/notification.js
+++ js/notification.js
@@ -1,3 +1,7 @@
+/**
+ * Send push notification to the client
+ * @param {string} message message for push notification
+ * @param {string} body description for push notification **/
 function notification(message, body) {
   const img = "../assets/logo.svg";
   const notification = new Notification(message, {
@@ -6,6 +10,9 @@ function notification(message, body) {
   });
 }
 
+/**
+ * Ask user whether to show the push notifcation.
+**/
 function requestNotification() {
   try {
     Notification.requestPermission().then(
--- js/utils/initializeStorage.js
+++ js/utils/initializeStorage.js
@@ -2,12 +2,12 @@
  * Initializes local storage.
  **/
 function initalizeStorage() {
-  localStorage.setItem("time-minutes", "25");
-  localStorage.setItem("time-seconds", "00");
-  localStorage.setItem("break-minutes", "05");
-  localStorage.setItem("break-seconds", "00");
-  localStorage.setItem("longbreak-minutes", "30");
-  localStorage.setItem("longbreak-seconds", "00");
+  localStorage.setItem("time-minutes", "0");
+  localStorage.setItem("time-seconds", "3");
+  localStorage.setItem("break-minutes", "0");
+  localStorage.setItem("break-seconds", "2");
+  localStorage.setItem("longbreak-minutes", "0");
+  localStorage.setItem("longbreak-seconds", "4");
   localStorage.setItem("initialize", "false")
 }
 
