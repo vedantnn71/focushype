29e9445c0a31affbcb5623ee675998c41970df2a
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-03-17 09:21:00 +0530

    Fix notification not coming, cache files in service work to make it work offline.

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,35 @@ importScripts(
 
 workbox.routing.registerRoute(
   ({ request }) => request.destination === "image",
-  new workbox.strategies.NetworkFirst()
+  new workbox.strategies.CacheFirst({
+    cacheName: "images",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ url }) =>
+    url.origin === "https://fonts.googleapis.com" ||
+    url.origin === "https://fonts.gstatic.com",
+
+  new StateWhileRevalidate({
+    cacheName: "google-fonts",
+    plugins: [new ExpirationPlugin({ maxEntries: 20 })],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ request }) =>
+    request.destination === "script" || request.destination === "style",
+  new StateWhileRevalidate()
 );
 
 self.addEventListener("install", function (event) {
