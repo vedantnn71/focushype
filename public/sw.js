96267ba7bc1dbc71d8b860ae0a0fbb0254a30a4d
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-04-03 10:15:28 +0530

    Fix caching

--- public/sw.js
+++ public/sw.js
@@ -1,41 +1,52 @@
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"
 );
 
 workbox.routing.registerRoute(
-  /\.html/,
+  ({ request }) => request.destination === "image",
   new workbox.strategies.CacheFirst({
-    cacheName: "html-cache",
-  })
-);
-
-workbox.routing.registerRoute(
-  /\.css$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: "css-cache",
-  })
-);
-
-workbox.routing.registerRoute(
-  /\.js/,
-  new workbox.strategies.CacheFirst({
-    cacheName: "js-cache",
-  })
-);
-
-workbox.routing.registerRoute(
-  /\.(?:png|jpg|jpeg|svg|gif)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: "image-cache",
+    cacheName: "images",
     plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20,
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 60,
         maxAgeSeconds: 7 * 24 * 60 * 60,
       }),
     ],
   })
 );
 
+workbox.routing.registerRoute(
+  ({ url }) =>
+    url.origin === "https://fonts.googleapis.com" ||
+    url.origin === "https://fonts.gstatic.com",
+
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "google-fonts",
+    plugins: [new workbox.expiration.ExpirationPlugin({ maxEntries: 20 })],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ request }) =>
+    request.destination === "script" || request.destination === "style",
+  new workbox.strategies.StaleWhileRevalidate()
+);
+
 self.addEventListener("install", function (event) {
   console.log("Installed!");
 });
+
+function matchURL({ url }) {
+  const pages = ["/", "/about"];
+  return pages.includes(url.pathname);
+}
+
+workbox.routing.registerRoute(
+  matchURL,
+  new workbox.strategies.CacheFirst({
+    cacheName: "html-cache",
+  })
+);
